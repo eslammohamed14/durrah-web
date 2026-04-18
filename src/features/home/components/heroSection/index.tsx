2f01b1b2885313ee7aeb176aa93884a8a8541bbc
ahmed-magdy34 <168830955+ahmed-magdy34@users.noreply.github.com>
2026-04-19 01:29:45 +0200

    enhace home section

--- src/features/home/components/heroSection/index.tsx
+++ src/features/home/components/heroSection/index.tsx
@@ -1,23 +1,17 @@
-import { cookies } from "next/headers";
 import Image from "next/image";
 import images from "@/constant/images";
 import { FilterContainer } from "./filter-container";
 import type { Property } from "@/lib/types";
-import { createTranslator, getBundledTranslations } from "@/lib/utils/i18n";
-import {
-  DEFAULT_LOCALE,
-  isValidLocale,
-  NEXT_LOCALE_COOKIE,
-} from "@/config/i18n";
+
+
 
 interface HeroSectionProps {
   allProperties: Property[];
+  t:any;
 }
 
-export async function HeroSection({ allProperties }: HeroSectionProps) {
-  const localeCookie = (await cookies()).get(NEXT_LOCALE_COOKIE)?.value;
-  const locale = isValidLocale(localeCookie) ? localeCookie : DEFAULT_LOCALE;
-  const t = createTranslator(locale, getBundledTranslations(locale));
+export const HeroSection = ({ allProperties,t }: HeroSectionProps) => {
+ 
 
   return (
     <section
@@ -30,6 +24,9 @@ export async function HeroSection({ allProperties }: HeroSectionProps) {
         alt="Hero background"
         fill
         priority
+        fetchPriority="high" // 🔥 ADD THIS
+        sizes="100vw"
+
         className="object-cover"
         style={{ objectPosition: "50% 25%" }}
       />
